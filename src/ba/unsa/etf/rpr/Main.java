052a1b6205787c4d92304b4ce603fbe27a26f912
MedinaKapo <mkapo2@etf.unsa.ba>
2019-10-17 18:50:34 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr;
+import java.util.Scanner;
+public class Main {
+    public static int SumaCifara(int n){
+        int suma=0;
+        while(n!=0){
+            suma+=n%10;
+            n/=10;
+        }
+        return suma;
+    }
+    public static void main(String[] args) {
+        int n;
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println("Unesite n: ");
+        n = ulaz.nextInt();
+        int suma=0;
+        suma=SumaCifara(n);
+        System.out.println("Suma svih cifara broja n je: "+suma);
+    }
+}
